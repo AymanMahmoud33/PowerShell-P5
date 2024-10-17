80503ea634522c4b0338942644e74ce81f1d609c
Ayman Aly Mahmoud <se.ayman@gmail.com>
2024-10-17 11:44:01 +0300

    Create LCM-Config.ps1

---
+++ DSC/LCM-Config.ps1
@@ -0,0 +1,21 @@
+
+# declare configuration
+Configuration LCMConfig {
+
+    # parameters
+    Param([string[]]$ComputerName = "localhost")
+
+    # target node
+    Node $ComputerName 
+    {
+        # LCM resource
+        LocalConfigurationManager
+        {
+            ConfigurationMode = "ApplyAndAutoCorrect"
+            ConfigurationModeFrequencyMins = 15
+        }
+    }
+}
+
+# generate .MOF files
+LCMConfig -ComputerName DC2
